8433eb2f24b4e83be31f2990b56b16b2508dd91f
ronething <axingfly@gmail.com>
2020-02-01 17:02:06 +0800

    fix: sample2

--- sample2.py
+++ sample2.py
@@ -27,21 +27,7 @@ async def cron_scheduler():
         # await job # 在当前协程中等待其他协程完成 会阻塞
         # 正确做法 create_task 将 job 分离出去 由 loop 调度
         a = asyncio.create_task(job)  # 注册到时间循环
-        # a = asyncio.ensure_future(job)
-        a.add_done_callback(functools.partial(print_res))
-        await asyncio.sleep(0)  # 主动让出资源调度
-        page = page + 1
-
-
-async def cron_scheduler1():
-    page = 1
-    while True:
-        # await asyncio.sleep(1)
-        job = cron_job1(F"https://google.com/{page}")
-        # await job # 在当前协程中等待其他协程完成 会阻塞
-        # 正确做法 create_task 将 job 分离出去 由 loop 调度
-        a = asyncio.create_task(job)  # 注册到时间循环
-        # a = asyncio.ensure_future(job)
+        # a = asyncio.ensure_future(job) # 跟 task 的作用类似 比 task api 级别底层
         a.add_done_callback(functools.partial(print_res))
         await asyncio.sleep(0)  # 主动让出资源调度
         page = page + 1
@@ -54,16 +40,8 @@ async def cron_job(i: str):
         return i
 
 
-async def cron_job1(i: str):
-    while True:
-        await asyncio.sleep(random.randint(1, 3))  # 网络延迟
-        # print("爬取网页中", i)
-        return i
-
-
 if __name__ == '__main__':
     c1 = cron_scheduler()
-    c2 = cron_scheduler1()
 
     loop = asyncio.get_event_loop()  # epoll
-    loop.run_until_complete(asyncio.gather(c1, c2))
+    loop.run_until_complete(c1)
